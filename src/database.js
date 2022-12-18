f0482abd7311e78c470c3dcab2e005ecd195fd8b
Miguel C <webdazer0@gmail.com>
2022-12-18 14:19:58 +0100

    chore: update package.json & fix moongose (last version)

--- src/database.js
+++ src/database.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .set("strictQuery", false)
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.log(err));
+    useUnifiedTopology: true,
+  })
+  .then((db) => console.log("DB is connected"))
+  .catch((err) => console.log(err));
